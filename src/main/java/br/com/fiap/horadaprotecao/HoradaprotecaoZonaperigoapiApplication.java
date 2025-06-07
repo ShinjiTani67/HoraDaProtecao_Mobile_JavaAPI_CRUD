4f9adc4401effedefc8d77100bf6f8430d686c47
ShinjiTani67 <144123611+ShinjiTani67@users.noreply.github.com>
2025-06-07 10:38:13 -0300

    add project

---
+++ src/main/java/br/com/fiap/horadaprotecao/HoradaprotecaoZonaperigoapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.horadaprotecao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoradaprotecaoZonaperigoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoradaprotecaoZonaperigoapiApplication.class, args);
+	}
+
+}
